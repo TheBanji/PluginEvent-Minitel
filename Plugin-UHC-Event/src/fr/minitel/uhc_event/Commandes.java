e357fa26b7196f89305ce47a38c7c648af91abd7
Banji <38909289+TheBanji@users.noreply.github.com>
2022-05-18 00:27:56 +0200

    Update Commandes.java

--- Plugin-UHC-Event/src/fr/minitel/uhc_event/Commandes.java
+++ Plugin-UHC-Event/src/fr/minitel/uhc_event/Commandes.java
@@ -16,26 +16,25 @@ public class Commandes implements CommandExecutor{
 			
 			if(command.equalsIgnoreCase("all")) {
 				if(length == 0) {
-					player.sendMessage("§a/all <msg> §9: §fEnvoi un broadcast à l'ensemble des joueurs.");
+					player.sendMessage("Â§a/all <msg> Â§9: Â§fEnvoi un broadcast Ã  l'ensemble des joueurs.");
 				} else {
 					StringBuilder str = new StringBuilder();
 					for(String s : args) {
 						str.append(s + " ");
 					}
-					Bukkit.broadcastMessage("§c[ADMIN]§f > §6" + str.toString());
+					Bukkit.broadcastMessage("Â§c[ADMIN]Â§f > Â§6" + str.toString());
 				}
 				return true;
 				
 			} else if(command.equalsIgnoreCase("go")) {
 				if(!VariablesGlobales.gameStarted) {
 					VariablesGlobales.gameStarted = true;
-					VariablesGlobales.registrationOn = false;
-					Bukkit.broadcastMessage("§aLa partie commence ! :p");
+					Bukkit.broadcastMessage("Â§aLa partie commence ! :p");
 					for(Player p : Bukkit.getServer().getOnlinePlayers()) {
-						p.sendTitle("§aGo ! Go !", "§6-= §cBonne chance §6=-", 10, 50, 20);
+						p.sendTitle("Â§aGo ! Go !", "Â§6-= Â§cBonne chance Â§6=-", 10, 50, 20);
 					}
 				} else {
-					player.sendMessage("§cLa partie est déjà commencée !");
+					player.sendMessage("Â§cLa partie est dÃ©jÃ  commencÃ©e !");
 				}
 				return true;
 			}
